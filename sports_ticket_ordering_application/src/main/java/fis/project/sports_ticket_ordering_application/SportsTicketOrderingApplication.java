d97b5096f4df4698ede3744599b87e930d04a30a
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-05-02 16:28:16 +0300

    Initial commit

---
+++ sports_ticket_ordering_application/src/main/java/fis/project/sports_ticket_ordering_application/SportsTicketOrderingApplication.java
@@ -0,0 +1,13 @@
+package fis.project.sports_ticket_ordering_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsTicketOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsTicketOrderingApplication.class, args);
+	}
+
+}
---
+++ sports_ticket_ordering_application/src/test/java/fis/project/sports_ticket_ordering_application/SportsTicketOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.project.sports_ticket_ordering_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsTicketOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
